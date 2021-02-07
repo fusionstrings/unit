5ece344aab5fe6e4cec31a85dc00bb0fce453e77
Dilip Kr. Shukla <contact@fusionstrings.com>
2021-02-07 18:30:38 +0100

    cleanup

--- www/app.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-require("unit-http").createServer(function (req, res) {
-	res.writeHead(200, { "Content-Type": "text/plain" });
-	res.end("Hello, Node.js on Unit!");
-}).listen()
